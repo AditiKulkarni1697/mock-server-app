b396c107127aec4954c39090739fec529d4df277
AditiSujlegaonkar1697 <aditisujlegaonkar@gmail.com>
2023-08-10 19:25:55 +0530

    added post route

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const jsonServer = require("json-server"); // importing json-server library
+
 var cors = require('cors')
+
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
 
 const server = jsonServer.create();
 
@@ -12,8 +19,22 @@ const port = process.env.PORT || 3000; // you can use any port number here; i ch
 server.use(middlewares);
 server.use(router);
 
-server.post("/",(req,res)=>{
-    res.send("posted")
+server.post("/",async(req,res)=>{
+    try{
+   // res.send("posted")
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+const file = join(__dirname, 'db.json')
+
+// Configure lowdb to write data to JSON file
+const adapter = new JSONFile(file)
+const defaultData = { posts: req.body }
+const db = new Low(adapter, defaultData)
+await db.write()
+res.send("posted")
+    }
+    catch(err){
+        console.log(err)
+    }
 })
 
 server.listen(port);
